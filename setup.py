b0a0d15b6e50290643b0a5ebecf75e17ebacf9ed
Matias Lindgren <matias.lindgren@gmail.com>
2019-08-20 01:03:33 +0300

    fix requirements for python 3.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "sox ~= 1.3",
         "webrtcvad ~= 2.0",
         "numpy < 1.17",
-        "matplotlib ~= 3.1",
+        "matplotlib ~= 3.0",
         # "tensorflow == 2.0.0-beta1",
         "tensorflow-gpu == 2.0.0-beta1",
     ],
