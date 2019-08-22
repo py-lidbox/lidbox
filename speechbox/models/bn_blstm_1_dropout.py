ff68a05751a57ec93f6b112d0d0de1be1ab78905
Matias Lindgren <matias.lindgren@gmail.com>
2019-08-22 22:50:51 +0300

    update models

--- speechbox/models/bn_blstm_1_dropout.py
+++ speechbox/models/bn_blstm_1_dropout.py
@@ -3,12 +3,16 @@ import tensorflow as tf
 
 def loader(input_shape, output_shape, num_cells=8, dropout=0.5, merge_mode='concat'):
     lstm = tf.keras.layers.LSTM(num_cells,
-        dropout=dropout,
+        dropout=dropout
     )
     blstm = tf.keras.layers.Bidirectional(lstm,
         name="BLSTM",
         merge_mode=merge_mode,
         input_shape=input_shape
     )
-    output = tf.keras.layers.Dense(output_shape, activation='softmax')
-    return tf.keras.models.Sequential([blstm, output])
+    bn = tf.keras.layers.BatchNormalization()
+    output = tf.keras.layers.Dense(output_shape,
+        name="softmax",
+        activation='softmax'
+    )
+    return tf.keras.models.Sequential([blstm, bn, output])
--- speechbox/models/lstm_1_dropout.py
+++ speechbox/models/lstm_1_dropout.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
 
 
-def loader(input_shape, output_shape, num_cells=8, dropout=0.5, recurrent_dropout=0.5):
-    return tf.keras.models.Sequential([
-        tf.keras.layers.LSTM(
-            num_cells,
-            input_shape=input_shape,
-            dropout=dropout,
-            recurrent_dropout=recurrent_dropout
-        ),
-        tf.keras.layers.Dense(output_shape, activation='softmax')
-    ])
+def loader(input_shape, output_shape, num_cells=8, dropout=0.5):
+    lstm = tf.keras.layers.LSTM(
+        num_cells,
+        input_shape=input_shape,
+        dropout=dropout
+    )
+    output = tf.keras.layers.Dense(output_shape, activation='softmax')
+    return tf.keras.models.Sequential([lstm, output])
