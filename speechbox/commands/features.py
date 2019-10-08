1a1894ae8c9345a58ba39e68178331f53920b5a3
Matias Lindgren <matias.lindgren@gmail.com>
2019-10-08 16:50:43 +0300

    extract features by label in parallel

--- speechbox/commands/features.py
+++ speechbox/commands/features.py
@@ -1,4 +1,6 @@
+import functools
 import itertools
+import multiprocessing
 import os
 import pprint
 import sys
@@ -6,6 +8,34 @@ import sys
 from speechbox.commands.base import State, Command, StatefulCommand
 import speechbox.system as system
 import speechbox.preprocess.transformations as transformations
+
+
+def extract_features_from_task(task):
+    """
+    Feature extractor function with a serializable parameter object for parallel and/or async execution.
+    """
+    (label, group), (tfrecords_dir, config, label_to_index) = task
+    labels = []
+    paths = []
+    for l, p in group:
+        labels.append(l)
+        paths.append(p)
+    assert all(l == label for l in labels), "Failed to group paths by label, expected all labels to be equal to '{}' but at least one was not".format(label)
+    utterances = transformations.speech_dataset_to_utterances(
+        labels, paths,
+        utterance_length_ms=config["utterance_length_ms"],
+        utterance_offset_ms=config["utterance_offset_ms"],
+        apply_vad=config.get("apply_vad", False),
+        print_progress=config.get("print_progress", 0),
+        slide_over_all=config.get("slide_over_all", True),
+    )
+    features = transformations.utterances_to_features(
+        utterances,
+        label_to_index=label_to_index,
+        extractors=config["extractors"],
+        sequence_length=config["sequence_length"]
+    )
+    return label, system.write_features(features, os.path.join(tfrecords_dir, label))
 
 
 class Features(Command):
@@ -16,6 +46,16 @@ class Features(Command):
 class Extract(StatefulCommand):
     """Extract features from audio files using parameters from the config yaml."""
     requires_state = State.has_paths
+
+    @classmethod
+    def create_argparser(cls, parent_parser):
+        parser = super().create_argparser(parent_parser)
+        optional = parser.add_argument_group("extract options")
+        optional.add_argument("--num-workers",
+            type=int,
+            default=1,
+            help="How many parallel processes to use when extracting features. Defaults to 1, which means multiprocessing will not be used.")
+        return parser
 
     def extract(self):
         args = self.args
@@ -28,7 +68,6 @@ class Extract(StatefulCommand):
             print("Using feature extraction parameters:")
             pprint.pprint(config)
             print()
-        label_to_index = self.state["label_to_index"]
         for datagroup_name in config["datagroups"]:
             datagroup = self.state["data"][datagroup_name]
             if args.verbosity:
@@ -49,29 +88,21 @@ class Extract(StatefulCommand):
                 print("Extracting features by label")
             # The zip is very important here so as not to mess up the ordering of label-path pairs
             paths_sorted_by_label = sorted(zip(datagroup["labels"], datagroup["paths"]))
-            # Extract all features by label, writing TFRecord files containing features for samples of only a single label
-            for label, group in itertools.groupby(paths_sorted_by_label, key=lambda pair: pair[0]):
-                labels, paths = list(zip(*group))
-                assert all(l == label for l in labels), "Failed to group paths by label, expected all labels to be equal to '{}' but at least one was not".format(label)
-                utterances = transformations.speech_dataset_to_utterances(
-                    labels, paths,
-                    utterance_length_ms=config["utterance_length_ms"],
-                    utterance_offset_ms=config["utterance_offset_ms"],
-                    apply_vad=config.get("apply_vad", False),
-                    print_progress=config.get("print_progress", 0),
-                    slide_over_all=config.get("slide_over_all", True),
-                )
-                features = transformations.utterances_to_features(
-                    utterances,
-                    label_to_index=label_to_index,
-                    extractors=config["extractors"],
-                    sequence_length=config["sequence_length"]
-                )
-                tfrecords_path = os.path.join(tfrecords_dir, label)
-                wrote_path = system.write_features(features, tfrecords_path)
+            # Group all paths by label
+            label_groups = itertools.groupby(paths_sorted_by_label, key=lambda pair: pair[0])
+            # We must evaluate the group generator since the key function is not picklable
+            label_groups = [(label, list(group)) for label, group in label_groups]
+            task_args = (tfrecords_dir, config, self.state["label_to_index"])
+            tasks = ((group, task_args) for group in label_groups)
+            if args.num_workers > 1:
+                with multiprocessing.Pool(args.num_workers) as pool:
+                    res = list(pool.imap_unordered(extract_features_from_task, tasks))
+            else:
+                res = [extract_features_from_task(t) for t in tasks]
+            for label, wrote_path in res:
                 datagroup["features"][label] = wrote_path
                 if args.verbosity:
-                    print("Wrote '{}' features to '{}'".format(datagroup_name, wrote_path))
+                    print("Wrote '{}' features for label '{}' into '{}'".format(datagroup_name, label, wrote_path))
 
     def run(self):
         super().run()
