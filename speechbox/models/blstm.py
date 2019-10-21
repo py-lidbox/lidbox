42297ea3746181edde646f5998092ce290db6737
Matias Lindgren <matias.lindgren@gmail.com>
2019-10-21 18:05:49 +0300

    add batch norm to blstm

--- speechbox/models/blstm.py
+++ speechbox/models/blstm.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def loader(input_shape, output_shape, num_units, num_layers=1, narrowing=False, merge_mode=None):
+def loader(input_shape, output_shape, num_units, num_layers=1, narrowing=False, merge_mode='sum', batch_normalize=False):
     blstm_layers = []
     lstm_1 = tf.keras.layers.LSTM(
         num_units,
@@ -9,9 +9,9 @@ def loader(input_shape, output_shape, num_units, num_layers=1, narrowing=False, 
     )
     blstm_1 = tf.keras.layers.Bidirectional(
         lstm_1,
-        merge_mode=merge_mode,
         input_shape=input_shape,
         name="BLSTM_1",
+        merge_mode=merge_mode,
     )
     blstm_layers.append(blstm_1)
     for i in range(2, num_layers + 1):
@@ -27,5 +27,11 @@ def loader(input_shape, output_shape, num_units, num_layers=1, narrowing=False, 
             name="BLSTM_{}".format(i)
         )
         blstm_layers.append(blstm_i)
+    if batch_normalize:
+        layers = []
+        for i, blstm in enumerate(blstm_layers, start=1):
+            bn = tf.keras.layers.BatchNormalization(name="batchnorm_{}".format(i))
+            layers.extend([blstm, bn])
+        blstm_layers = layers
     output = tf.keras.layers.Dense(output_shape, activation='softmax')
     return tf.keras.models.Sequential(blstm_layers + [output])
