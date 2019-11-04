1f76fe0f8be896f32442298076348c7f3762db70
Matias Lindgren <matias.lindgren@gmail.com>
2019-11-04 23:42:25 +0200

    add spectrogram plot for tensorboard

--- speechbox/librosa_tf.py
+++ speechbox/librosa_tf.py
@@ -1,7 +1,12 @@
 """
 Some functions from https://github.com/librosa converted to TensorFlow math.
 """
+import io
+
+import matplotlib.pyplot as plt
+import seaborn
 import tensorflow as tf
+
 
 @tf.function
 def power_to_db(S, ref=tf.math.reduce_max, amin=1e-10, top_db=80.0):
@@ -10,13 +15,43 @@ def power_to_db(S, ref=tf.math.reduce_max, amin=1e-10, top_db=80.0):
     return tf.math.maximum(log_spec, tf.math.reduce_max(log_spec) - top_db)
 
 @tf.function
-def spectrogram(signal, n_fft=2048, hop_length=512, power=1.0):
-    S = tf.math.abs(tf.signal.stft(signal, n_fft, hop_length, fft_length=n_fft))
+def spectrograms(signals, n_fft=512, hop_length=160, power=2.0):
+    S = tf.math.abs(tf.signal.stft(signals, n_fft, hop_length, fft_length=n_fft))
     return tf.math.pow(S, power)
 
 @tf.function
-def melspectrogram(signal, sample_rate, num_mel_bins=128):
-    S = spectrogram(signal)
-    W_mel = tf.signal.linear_to_mel_weight_matrix(sample_rate=sample_rate, num_spectrogram_bins=S.shape[1], num_mel_bins=num_mel_bins)
-    print(S.shape, W_mel.shape)
-    return power_to_db(tf.matmul(S, W_mel))
+def melspectrograms(signals, sample_rate, num_mel_bins=40, fmin=60.0, fmax=6000.0):
+    S = spectrograms(signals)
+    mel_weights = tf.signal.linear_to_mel_weight_matrix(
+        num_mel_bins=num_mel_bins,
+        num_spectrogram_bins=S.shape[-1],
+        sample_rate=sample_rate,
+        lower_edge_hertz=fmin,
+        upper_edge_hertz=fmax,
+    )
+    return tf.matmul(S, mel_weights)
+
+@tf.function
+def mfcc(melspectrogram, n_coefs=13):
+    log_mel_spectrogram = tf.math.log(melspectrogram + 1e-6)
+    return tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[..., 1:n_coefs]
+
+def get_heatmap_plot(batch):
+    figure = plt.figure(figsize=(10, 10))
+    width = 5
+    height = batch.shape[1] // width + (batch.shape[1] % width) != 0
+    for i, data in enumerate(batch, start=1):
+        ax = plt.subplot(height, width, i)
+        ax.set_title(str(i))
+        ax.invert_yaxis()
+        seaborn.heatmap(data.T, ax=ax, cmap="YlGnBu_r")
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    figure.close()
+    buf.seek(0)
+    heatmaps_png = tf.io.decode_png(buf.getvalue(), channels=4)
+    return heatmaps_png
+
+def get_spectrogram_plot_from_signal(signals, sample_rate):
+    melspecs_db = power_to_db(melspectrogram(signals, sample_rate))
+    return get_heatmap_plot(melspecs_db)
