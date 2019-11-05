1d2098ef3203715ef117748fb5c2c01d242c99e2
Matias Lindgren <matias.lindgren@gmail.com>
2019-11-05 17:06:38 +0200

    add rms energy vad

--- speechbox/librosa_tf.py
+++ speechbox/librosa_tf.py
@@ -15,13 +15,12 @@ def power_to_db(S, ref=tf.math.reduce_max, amin=1e-10, top_db=80.0):
     return tf.math.maximum(log_spec, tf.math.reduce_max(log_spec) - top_db)
 
 @tf.function
-def spectrograms(signals, n_fft=512, hop_length=160, power=2.0):
+def spectrograms(signals, n_fft=400, hop_length=160, power=2.0):
     S = tf.math.abs(tf.signal.stft(signals, n_fft, hop_length, fft_length=n_fft))
     return tf.math.pow(S, power)
 
 @tf.function
-def melspectrograms(signals, sample_rate, num_mel_bins=40, fmin=60.0, fmax=6000.0):
-    S = spectrograms(signals)
+def melspectrograms(S, sample_rate, num_mel_bins=40, fmin=60.0, fmax=6000.0):
     mel_weights = tf.signal.linear_to_mel_weight_matrix(
         num_mel_bins=num_mel_bins,
         num_spectrogram_bins=S.shape[-1],
@@ -35,6 +34,25 @@ def melspectrograms(signals, sample_rate, num_mel_bins=40, fmin=60.0, fmax=6000.
 def mfcc(melspectrogram, n_coefs=13):
     log_mel_spectrogram = tf.math.log(melspectrogram + 1e-6)
     return tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[..., 1:n_coefs]
+
+@tf.function
+def energy_vad(signals, n_fft=400, hop_length=160, vad_strength=0.7):
+    """Returns frame-wise vad decisions."""
+    tf.debugging.assert_non_negative(vad_strength)
+    frames = tf.signal.frame(signals, n_fft, hop_length)
+    rms = tf.math.sqrt(tf.math.reduce_mean(tf.math.square(tf.math.abs(frames)), axis=2))
+    threshold = vad_strength * 0.5 * tf.math.reduce_mean(rms, axis=1, keepdims=True)
+    return tf.math.greater(rms, threshold)
+
+@tf.function
+def extract_melspec_features(signals, sample_rate):
+    melspecs = melspectrograms(spectrograms(signals), sample_rate)
+    vad_decisions = energy_vad(signals)
+    return tf.ragged.boolean_mask(melspecs, vad_decisions)
+
+#FIXME
+def read_wavs(paths):
+    wavs = tf.ragged.constant([read(p) for p in paths], dtype=tf.float32)
 
 def get_heatmap_plot(batch):
     figure = plt.figure(figsize=(10, 10))
