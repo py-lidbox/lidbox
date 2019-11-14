7de5db2958e23677b921c14446842930a61e9457
Matias Lindgren <matias.lindgren@gmail.com>
2019-11-14 16:39:31 +0200

    expand crnn2 input shape to singleton channels

--- speechbox/models/crnn2.py
+++ speechbox/models/crnn2.py
@@ -12,19 +12,20 @@ from tensorflow.keras.models import Sequential
 
 def loader(input_shape, output_shape, num_lstm_units=256):
     return Sequential([
-         Conv2D(64, 7, 2, input_shape=input_shape, padding="same"),
-         MaxPool2D(2),
-         Conv2D(64, 3, 2, padding="same"),
-         BatchNormalization(),
-         Conv2D(128, 3, 2, padding="same"),
-         BatchNormalization(),
-         Conv2D(256, 3, 2, padding="same"),
-         BatchNormalization(),
-         Conv2D(512, 3, 2, padding="same"),
-         BatchNormalization(),
-         Permute((3, 1, 2)),
-         Reshape((-1, 1)),
-         Bidirectional(LSTM(num_lstm_units, return_sequences=True)),
-         Bidirectional(LSTM(num_lstm_units)),
-         Dense(output_shape, activation="softmax")
+        Reshape(input_shape + (1, ), input_shape=input_shape),
+        Conv2D(64, 7, 2, padding="same"),
+        MaxPool2D(2),
+        Conv2D(64, 3, 2, padding="same"),
+        BatchNormalization(),
+        Conv2D(128, 3, 2, padding="same"),
+        BatchNormalization(),
+        Conv2D(256, 3, 2, padding="same"),
+        BatchNormalization(),
+        Conv2D(512, 3, 2, padding="same"),
+        BatchNormalization(),
+        Permute((3, 1, 2)),
+        Reshape((-1, 1)),
+        Bidirectional(LSTM(num_lstm_units, return_sequences=True)),
+        Bidirectional(LSTM(num_lstm_units)),
+        Dense(output_shape, activation="softmax")
      ])
