d9866ca36aef6d11b69acc5ae9a17ca04e153e93
Matias Lindgren <matias.lindgren@gmail.com>
2019-11-20 17:11:38 +0200

    add names to cnn layers

--- speechbox/models/cnn.py
+++ speechbox/models/cnn.py
@@ -10,20 +10,20 @@ from tensorflow.keras.layers import (
 )
 from tensorflow.keras import Sequential
 
-def loader(input_shape, output_shape, d=1):
+def loader(input_shape, num_outputs, d=1):
     return Sequential([
-        Conv1D(500//d, 5, 1, input_shape=input_shape, padding="same", activation="relu"),
-        BatchNormalization(),
-        Conv1D(500//d, 7, 2, padding="same", activation="relu"),
-        BatchNormalization(),
-        Conv1D(500//d, 1, 1, padding="same", activation="relu"),
-        BatchNormalization(),
-        Conv1D(3000//d, 1, 1, padding="same", activation="relu"),
-        BatchNormalization(),
-        GlobalAveragePooling1D(),
-        Dense(1500//d, activation="relu"),
-        BatchNormalization(),
-        Dense(600//d, activation="relu"),
-        BatchNormalization(),
-        Dense(output_shape, activation="softmax"),
+        Conv1D(500//d, 5, 1, input_shape=input_shape, padding="same", activation="relu", name="conv_1"),
+        BatchNormalization(name="BN_conv_1"),
+        Conv1D(500//d, 7, 2, padding="same", activation="relu", name="conv_2"),
+        BatchNormalization(name="BN_conv_2"),
+        Conv1D(500//d, 1, 1, padding="same", activation="relu", name="conv_3"),
+        BatchNormalization(name="BN_conv_3"),
+        Conv1D(3000//d, 1, 1, padding="same", activation="relu", name="conv_4"),
+        BatchNormalization(name="BN_conv_4"),
+        GlobalAveragePooling1D(name="conv_4_pooling"),
+        Dense(1500//d, activation="relu", name="dense_1"),
+        BatchNormalization(name="BN_dense_1"),
+        Dense(600//d, activation="relu", name="dense_2"),
+        BatchNormalization(name="BN_dense_2"),
+        Dense(num_outputs, activation="softmax", name="output"),
     ])
