5442143ed3054aa3c190c531f3038d335c7d5d08
Matias Lindgren <matias.lindgren@gmail.com>
2019-12-05 18:02:37 +0200

    update cnn to resemble more the original

--- speechbox/models/cnn.py
+++ speechbox/models/cnn.py
@@ -9,6 +9,7 @@ from tensorflow.keras.layers import (
     Conv1D,
     Dense,
     GlobalAveragePooling1D,
+    ReLU,
 )
 from tensorflow.keras import Sequential
 
@@ -16,18 +17,24 @@ def loader(input_shape, num_outputs, variable_input_length=False):
     if variable_input_length:
         input_shape = (None, *input_shape[1:])
     return Sequential([
-        Conv1D(500, 5, 1, input_shape=input_shape, padding="same", activation="relu", name="conv_1"),
+        Conv1D(500, 5, 1, input_shape=input_shape, padding="same", activation=None, name="conv_1"),
         BatchNormalization(name="BN_conv_1"),
-        Conv1D(500, 7, 2, padding="same", activation="relu", name="conv_2"),
+        ReLU(name="conv_1_relu"),
+        Conv1D(500, 7, 2, padding="same", activation=None, name="conv_2"),
         BatchNormalization(name="BN_conv_2"),
-        Conv1D(500, 1, 1, padding="same", activation="relu", name="conv_3"),
+        ReLU(name="conv_2_relu"),
+        Conv1D(500, 1, 1, padding="same", activation=None, name="conv_3"),
         BatchNormalization(name="BN_conv_3"),
-        Conv1D(3000, 1, 1, padding="same", activation="relu", name="conv_4"),
+        ReLU(name="conv_3_relu"),
+        Conv1D(3000, 1, 1, padding="same", activation=None, name="conv_4"),
         BatchNormalization(name="BN_conv_4"),
+        ReLU(name="conv_4_relu"),
         GlobalAveragePooling1D(name="conv_4_pooling"),
-        Dense(1500, activation="relu", name="dense_1"),
+        Dense(1500, activation=None, name="dense_1"),
         BatchNormalization(name="BN_dense_1"),
-        Dense(600, activation="relu", name="dense_2"),
+        ReLU(name="dense_1_relu"),
+        Dense(600, activation=None, name="dense_2"),
         BatchNormalization(name="BN_dense_2"),
-        Dense(num_outputs, activation="softmax", name="output"),
+        ReLU(name="dense_2_relu"),
+        Dense(num_outputs, activation=None, name="output"),
     ])
