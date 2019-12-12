3026af99ad52e6d257bfb9b8d399c623858b3115
Matias Lindgren <matias.lindgren@gmail.com>
2019-12-12 18:54:55 +0200

    remove batch norm from cnn

--- speechbox/models/cnn.py
+++ speechbox/models/cnn.py
@@ -5,36 +5,23 @@ See also
 https://github.com/swshon/dialectID_e2e/blob/20bec9bea05747fcf4845921498f8b3abf52c7c6/models/e2e_model.py
 """
 from tensorflow.keras.layers import (
-    BatchNormalization,
     Conv1D,
     Dense,
     GlobalAveragePooling1D,
-    ReLU,
 )
 from tensorflow.keras import Sequential
 
-def loader(input_shape, num_outputs, variable_input_length=False):
-    if variable_input_length:
-        input_shape = (None, *input_shape[1:])
+def loader(input_shape, num_outputs):
     return Sequential([
-        Conv1D(500, 5, 1, input_shape=input_shape, padding="same", activation=None, name="conv_1"),
-        BatchNormalization(name="BN_conv_1"),
-        ReLU(name="conv_1_relu"),
-        Conv1D(500, 7, 2, padding="same", activation=None, name="conv_2"),
-        BatchNormalization(name="BN_conv_2"),
-        ReLU(name="conv_2_relu"),
-        Conv1D(500, 1, 1, padding="same", activation=None, name="conv_3"),
-        BatchNormalization(name="BN_conv_3"),
-        ReLU(name="conv_3_relu"),
-        Conv1D(3000, 1, 1, padding="same", activation=None, name="conv_4"),
-        BatchNormalization(name="BN_conv_4"),
-        ReLU(name="conv_4_relu"),
-        GlobalAveragePooling1D(name="conv_4_pooling"),
-        Dense(1500, activation=None, name="dense_1"),
-        BatchNormalization(name="BN_dense_1"),
-        ReLU(name="dense_1_relu"),
-        Dense(600, activation=None, name="dense_2"),
-        BatchNormalization(name="BN_dense_2"),
-        ReLU(name="dense_2_relu"),
-        Dense(num_outputs, activation=None, name="output"),
+        Conv1D(500, 5, 1, input_shape=input_shape, padding="same", activation="relu", name="conv_1"),
+        Conv1D(500, 7, 2, padding="same", activation="relu", name="conv_2"),
+        Conv1D(500, 1, 1, padding="same", activation="relu", name="conv_3"),
+        Conv1D(3000, 1, 1, padding="same", activation="relu", name="conv_4"),
+        GlobalAveragePooling1D(name="avg_pooling"),
+        Dense(1500, activation="relu", name="fc_1"),
+        Dense(600, activation="relu", name="fc_2"),
+        Dense(num_outputs, activation="softmax", name="output"),
     ])
+
+def predict(model, samples):
+    return samples
