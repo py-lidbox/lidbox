9124b0e723b9aba5828c6091ca32d66579e438a5
Matias Lindgren <matias.lindgren@gmail.com>
2020-01-02 23:34:19 +0200

    use only conv1d for xvector for now

--- speechbox/models/xvector.py
+++ speechbox/models/xvector.py
@@ -7,14 +7,12 @@ http://danielpovey.com/files/2018_odyssey_xvector_lid.pdf
 from tensorflow.keras.layers import (
     Conv1D,
     Dense,
+    Input,
     Layer,
     ReLU,
 )
 from tensorflow.keras.models import Sequential
 import tensorflow as tf
-
-from .tdnn import TDNN
-
 
 xvector_layer_names = ["frame{:d}".format(i) for i in range(1, 6)] + ["stats_pooling", "segment6"]
 
@@ -27,29 +25,18 @@ class GlobalMeanStddevPooling1D(Layer):
         std = tf.math.reduce_std(inputs, axis=steps_axis)
         return tf.concat((mean, std), axis=1)
 
-def loader(input_shape, num_outputs, output_activation="softmax", use_conv1d_for_tdnn=True):
-    if use_conv1d_for_tdnn:
-        tdnn = [
-            Conv1D(512, 5, 1, padding="valid", activation="relu", name="frame1", input_shape=input_shape),
-            Conv1D(512, 3, 2, padding="valid", activation="relu", name="frame2"),
-            Conv1D(512, 3, 3, padding="valid", activation="relu", name="frame3"),
-            Conv1D(512, 1, 1, padding="valid", activation="relu", name="frame4"),
-            Conv1D(1500, 1, 1, padding="valid", activation="relu", name="frame5"),
-        ]
-    else:
-        tdnn = [
-            TDNN(512, [-2, -1, 0, 1, 2], activation="relu", name="frame1", input_shape=input_shape),
-            TDNN(512, [-2, 0, 2], activation="relu", name="frame2"),
-            TDNN(512, [-3, 0, 3], activation="relu", name="frame3"),
-            TDNN(512, [0], activation="relu", name="frame4"),
-            TDNN(1500, [0], activation="relu", name="frame5"),
-        ]
-    return Sequential(tdnn + [
+def loader(input_shape, num_outputs, output_activation="softmax"):
+    return Sequential([
+        Conv1D( 512, 5, 1, name="frame1", padding="valid", activation="relu", input_shape=input_shape),
+        Conv1D( 512, 3, 2, name="frame2", padding="valid", activation="relu"),
+        Conv1D( 512, 3, 3, name="frame3", padding="valid", activation="relu"),
+        Conv1D( 512, 1, 1, name="frame4", padding="valid", activation="relu"),
+        Conv1D(1500, 1, 1, name="frame5", padding="valid", activation="relu"),
         GlobalMeanStddevPooling1D(name="stats_pooling"),
-        Dense(512, activation=None, name="segment6"),
+        Dense(512, name="segment6", activation=None),
         ReLU(name="segment6_relu"),
-        Dense(512, activation="relu", name="segment7"),
-        Dense(num_outputs, activation=output_activation, name="output"),
+        Dense(512, name="segment7", activation="relu"),
+        Dense(num_outputs, name="output", activation=output_activation),
     ])
 
 def extract_xvectors(model, inputs):
