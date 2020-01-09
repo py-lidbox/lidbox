932e28f4d073967ac2767f3f893963b954f84d0c
Matias Lindgren <matias.lindgren@gmail.com>
2020-01-09 13:47:19 +0200

    implement separate xvector layers

--- speechbox/models/xvector.py
+++ speechbox/models/xvector.py
@@ -5,17 +5,18 @@ David Snyder, et al. "Spoken Language Recognition using X-vectors." Odyssey. 201
 http://danielpovey.com/files/2018_odyssey_xvector_lid.pdf
 """
 from tensorflow.keras.layers import (
+    Activation,
     BatchNormalization,
     Conv1D,
     Dense,
+    Dropout,
     Input,
     Layer,
     ReLU,
 )
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
 import tensorflow as tf
 
-xvector_layer_names = ["frame{:d}".format(i) for i in range(1, 6)] + ["stats_pooling", "segment6"]
 
 class GlobalMeanStddevPooling1D(Layer):
     """Compute arithmetic mean and standard deviation of the inputs over the time dimension, then output the concatenation of the computed stats."""
@@ -26,52 +27,87 @@ class GlobalMeanStddevPooling1D(Layer):
         std = tf.math.reduce_std(inputs, axis=steps_axis)
         return tf.concat((mean, std), axis=1)
 
-def loader(input_shape, num_outputs, output_activation="softmax", batch_normalization=False):
-    if batch_normalization:
-        model = Sequential([
-            Conv1D(512, 5, 1, name="frame1", padding="valid", activation=None, input_shape=input_shape),
-            BatchNormalization(name="frame1_bn"),
-            ReLU(name="frame1_relu"),
-            Conv1D(512, 3, 2, name="frame2", padding="valid", activation=None),
-            BatchNormalization(name="frame2_bn"),
-            ReLU(name="frame2_relu"),
-            Conv1D(512, 3, 3, name="frame3", padding="valid", activation=None),
-            BatchNormalization(name="frame3_bn"),
-            ReLU(name="frame3_relu"),
-            Conv1D(512, 1, 1, name="frame4", padding="valid", activation=None),
-            BatchNormalization(name="frame4_bn"),
-            ReLU(name="frame4_relu"),
-            Conv1D(1500, 1, 1, name="frame5", padding="valid", activation=None),
-            BatchNormalization(name="frame5_bn"),
-            ReLU(name="frame5_relu"),
-            GlobalMeanStddevPooling1D(name="stats_pooling"),
-            Dense(512, name="segment6", activation=None),
-            BatchNormalization(name="segment6_bn"),
-            ReLU(name="segment6_relu"),
-            Dense(512, name="segment7", activation=None),
-            BatchNormalization(name="segment7_bn"),
-            ReLU(name="segment7_relu"),
-        ])
-    else:
-        model = Sequential([
-            Conv1D(512, 5, 1, name="frame1", padding="valid", activation="relu", input_shape=input_shape),
-            Conv1D(512, 3, 2, name="frame2", padding="valid", activation="relu"),
-            Conv1D(512, 3, 3, name="frame3", padding="valid", activation="relu"),
-            Conv1D(512, 1, 1, name="frame4", padding="valid", activation="relu"),
-            Conv1D(1500, 1, 1, name="frame5", padding="valid", activation="relu"),
-            GlobalMeanStddevPooling1D(name="stats_pooling"),
-            Dense(512, name="segment6", activation=None),
-            ReLU(name="segment6_relu"),
-            Dense(512, name="segment7", activation="relu"),
-        ])
-    model.add(Dense(num_outputs, name="output", activation=output_activation))
-    return model
 
-def extract_xvectors(model, inputs):
-    """Generator that embeds all elements of a tf.data.Dataset as X-vectors using the given, pretrained Sequential instance returned by 'loader'."""
-    xvector_extractor = Sequential([model.get_layer(l) for l in xvector_layer_names])
-    for x in inputs:
-        yield xvector_extractor(x)
+class FrameLayer(Layer):
+    def __init__(self, filters, kernel_size, stride, name="resnet", activation="relu", padding="causal", dropout_rate=None):
+        super().__init__(name=name)
+        self.conv = Conv1D(filters, kernel_size, stride, name="{}_conv".format(name), activation=None, padding=padding)
+        self.batch_norm = BatchNormalization(name="{}_bn".format(name))
+        self.activation = Activation(activation, name="{}_{}".format(name, str(activation)))
+        self.dropout = None
+        if dropout_rate:
+            self.dropout = Dropout(rate=dropout_rate, name="{}_dropout".format(name))
+
+    def call(self, inputs, training=False):
+        x = self.conv(inputs)
+        x = self.batch_norm(x, training=training)
+        x = self.activation(x)
+        if self.dropout:
+            x = self.dropout(x)
+        return x
+
+    def get_config(self):
+        return {
+            "filters": self.conv.filters,
+            "kernel_size": self.conv.kernel_size[0],
+            "stride": self.conv.strides[0],
+            "name": super().get_config()["name"],
+            "activation": self.activation.activation,
+            "padding": self.conv.padding,
+            "dropout": self.dropout.rate if self.dropout else None
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class SegmentLayer(Layer):
+    def __init__(self, units, name="segment", activation="relu", dropout_rate=None):
+        super().__init__(name=name)
+        self.dense = Dense(units, name="{}_dense".format(name), activation=None)
+        self.batch_norm = BatchNormalization(name="{}_bn".format(name))
+        self.activation = Activation(activation, name="{}_{}".format(name, str(activation)))
+        self.dropout = None
+        if dropout_rate:
+            self.dropout = Dropout(rate=dropout_rate, name="{}_dropout".format(name))
+
+    def call(self, inputs, training=False):
+        x = self.dense(inputs)
+        x = self.batch_norm(x, training=training)
+        x = self.activation(x)
+        if self.dropout:
+            x = self.dropout(x)
+        return x
+
+    def get_config(self):
+        return {
+            "units": self.dense.units,
+            "name": super().get_config()["name"],
+            "activation": self.activation.activation,
+            "dropout": self.dropout.rate if self.dropout else None
+        }
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+def loader(input_shape, num_outputs, output_activation="softmax"):
+    output_activation_fn = getattr(tf.nn, output_activation) if output_activation is not None else None
+    inputs = Input(shape=input_shape, name="input")
+    frame1 = FrameLayer(512, 5, 1, name="frame1")(inputs)
+    frame2 = FrameLayer(512, 3, 2, name="frame2")(frame1)
+    frame3 = FrameLayer(512, 3, 3, name="frame3")(frame2)
+    frame4 = FrameLayer(512, 1, 1, name="frame4")(frame3)
+    frame5 = FrameLayer(1500, 1, 1, name="frame5")(frame4)
+    stats_pooling = GlobalMeanStddevPooling1D(name="stats_pooling")(frame5)
+    segment1 = SegmentLayer(512, name="segment1")(stats_pooling)
+    segment2 = SegmentLayer(512, name="segment2")(segment1)
+    outputs = Dense(num_outputs, name="output", activation=None)(segment2)
+    outputs = Activation(output_activation_fn, name=str(output_activation))(outputs)
+    return Model(inputs=inputs, outputs=outputs)
+
 
 def predict(model, inputs):
     return model.predict(inputs)
