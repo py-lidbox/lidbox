cb3720222bacb3bffd2ba91092f6ce60df8be2f2
Matias Lindgren <matias.lindgren@gmail.com>
2020-01-10 19:52:08 +0200

    update models

--- speechbox/models/bi_gru.py
+++ speechbox/models/bi_gru.py
@@ -3,20 +3,26 @@ bi-GRU RNN used by
 Mateju, Lukas et al. (2018). "Using Deep Neural Networks for Identification of Slavic Languages from Acoustic Signal". In: Proc. Interspeech 2018, pp. 1803–1807. DOI: 10.21437/Interspeech.2018-1165. URL: http://dx.doi.org/10.21437/Interspeech.2018-1165.
 """
 from tensorflow.keras.layers import (
+    BatchNormalization,
     Bidirectional,
     Dense,
     GRU,
+    Input,
 )
-from tensorflow.keras import Sequential
+from tensorflow.keras import Model
 import numpy as np
 
-def loader(input_shape, num_outputs, merge_mode="concat", num_gru_units=1024):
-    return Sequential([
-        Bidirectional(GRU(num_gru_units, return_sequences=True), input_shape=input_shape, merge_mode=merge_mode, name="biGRU_1"),
-        Bidirectional(GRU(num_gru_units), merge_mode=merge_mode, name="biGRU_2"),
-        Dense(1024, activation="relu", name="fc_relu"),
-        Dense(num_outputs, activation="softmax", name="output"),
-    ])
+def loader(input_shape, num_outputs, merge_mode="concat", num_gru_units=1024, batch_normalize=False):
+    inputs = Input(shape=input_shape, name="input")
+    bigru1 = Bidirectional(GRU(num_gru_units, return_sequences=True), merge_mode=merge_mode, name="biGRU_1")(inputs)
+    bigru2 = Bidirectional(GRU(num_gru_units), merge_mode=merge_mode, name="biGRU_2")(bigru2)
+    if batch_normalize:
+        bigru2 = BatchNormalization(name="biGRU_2_bn")(bigru2)
+    fc_relu1 = Dense(1024, activation="relu", name="fc_relu")(bigru2)
+    if batch_normalize:
+        fc_relu1 = BatchNormalization(name="fc_relu_bn")(fc_relu1)
+    outputs = Dense(num_outputs, activation="softmax", name="output")(fc_relu1)
+    return Model(inputs=inputs, outputs=outputs)
 
 def predict(model, utterances):
     return np.stack([model.predict(frames).mean(axis=0) for frames in utterances.unbatch()])
--- speechbox/models/cnn.py
+++ speechbox/models/cnn.py
@@ -1,5 +1,5 @@
 """
-CNN + average pooling for variable length input utterances from
+CNN + average pooling for variable length input utterances by
 Suwon Shon, Ahmed Ali, and James Glass (June 2018). “Convolutional Neural Network and Language Embeddings for End-to-End Dialect Recognition”.
 See also
 https://github.com/swshon/dialectID_e2e/blob/20bec9bea05747fcf4845921498f8b3abf52c7c6/models/e2e_model.py
@@ -12,6 +12,9 @@ from tensorflow.keras.layers import (
 from tensorflow.keras import Sequential
 
 def loader(input_shape, num_outputs, output_activation="softmax", padding="same"):
+    output_activation_fn = None
+    if output_activation in ("log_softmax",):
+        output_activation_fn = getattr(tf.nn, output_activation)
     return Sequential([
         Conv1D(500, 5, 1, padding=padding, activation="relu", name="conv_1", input_shape=input_shape),
         Conv1D(500, 7, 2, padding=padding, activation="relu", name="conv_2"),
@@ -20,7 +23,7 @@ def loader(input_shape, num_outputs, output_activation="softmax", padding="same"
         GlobalAveragePooling1D(name="avg_pooling"),
         Dense(1500, activation="relu", name="fc_1"),
         Dense(600, activation="relu", name="fc_2"),
-        Dense(num_outputs, activation=output_activation, name="output"),
+        Dense(num_outputs, activation=output_activation_fn, name="output_{}".format(str(output_activation))),
     ])
 
 def predict(model, samples):
