a3f62d8321107f0eb2028dda4de9acec8245ff54
Matias Lindgren <matias.lindgren@gmail.com>
2020-02-11 20:37:12 +0200

    write profiler output even when exceptions occur

--- lidbox/__main__.py
+++ lidbox/__main__.py
@@ -29,15 +29,18 @@ def main():
         tf_profile_file = os.path.abspath(os.path.join("tf_profile", str(int(time.time()))))
         print("Running TensorFlow profiler, writing output to '{}'".format(tf_profile_file), file=sys.stderr)
         tf_profiler.start()
-    # Initialize a Command object from the class specified in args.cmd_class and remove the class from args
-    command = args.__dict__.pop("cmd_class")(args)
-    ret = command.run() or command.exit()
-    if tf_profiler:
-        tf_profiler_result = tf_profiler.stop()
-        tf_profiler.save(tf_profile_file, tf_profiler_result)
-    if profile:
-        profile.disable()
-        with open(profile_file, "w") as out_f:
-            pstats.Stats(profile, stream=out_f).sort_stats("tottime").print_stats()
+    ret = 1
+    try:
+        # Initialize a Command object from the class specified in args.cmd_class and remove the class from args
+        command = args.__dict__.pop("cmd_class")(args)
+        ret = command.run() or command.exit()
+    finally:
+        if tf_profiler:
+            tf_profiler_result = tf_profiler.stop()
+            tf_profiler.save(tf_profile_file, tf_profiler_result)
+        if profile:
+            profile.disable()
+            with open(profile_file, "w") as out_f:
+                pstats.Stats(profile, stream=out_f).sort_stats("tottime").print_stats()
     if ret:
         sys.exit(ret)
