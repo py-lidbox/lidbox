376292ab05a304dd9fc2b672aba994adf0a767e5
Matias Lindgren <matias.lindgren@gmail.com>
2020-03-22 21:17:15 +0200

    add simple lstm

---
+++ lidbox/models/lstm.py
@@ -0,0 +1,24 @@
+"""
+Just a single-layer LSTM for softmax classification.
+"""
+from tensorflow.keras.layers import (
+    Activation,
+    Dense,
+    Input,
+    LSTM,
+)
+from tensorflow.keras.models import Model
+import numpy as np
+import tensorflow as tf
+
+
+def loader(input_shape, num_outputs, output_activation="log_softmax", num_units=1024):
+    inputs = Input(shape=input_shape, name="input")
+    lstm = LSTM(num_units, name="lstm")(inputs)
+    outputs = Dense(num_outputs, name="output", activation=None)(lstm)
+    if output_activation:
+        outputs = Activation(getattr(tf.nn, output_activation), name=str(output_activation))(outputs)
+    return Model(inputs=inputs, outputs=outputs, name="lstm")
+
+def predict(model, utterances):
+    return np.stack([model.predict(frames).mean(axis=0) for frames in utterances.unbatch()])
