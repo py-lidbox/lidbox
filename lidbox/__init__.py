6276752f6a0c82c64e352564f71d7d6c2811b2e5
Matias Lindgren <matias.lindgren@gmail.com>
2020-11-23 00:01:18 +0200

    cleanup obsolete code

--- lidbox/__init__.py
+++ lidbox/__init__.py
@@ -7,8 +7,6 @@ import os
 import random
 import sys
 
-import yaml
-
 
 random.seed(int(os.environ.get("LIDBOX_RANDOM_SEED", "42")))
 
@@ -39,23 +37,6 @@ reset_global_loglevel(logging.DEBUG if DEBUG else logging.INFO)
 def get_package_root():
     from . import __path__
     return os.path.abspath(os.path.dirname(__path__[0]))
-
-def yaml_pprint(d, left_pad=0, to_string=False, **kwargs):
-    assert not to_string or "file" not in kwargs, "'to_string' specified with 'file' in kwargs for yaml_pprint, writing to string stream would override the other stream"
-    def _do_print():
-        for line in yaml.dump(d, indent=2).splitlines():
-            print(left_pad * ' ', line, sep='', **kwargs)
-    if to_string:
-        with io.StringIO() as sstream:
-            kwargs["file"] = sstream
-            _do_print()
-            return sstream.getvalue()
-    else:
-        _do_print()
-
-def load_yaml(path):
-    with open(path, encoding="utf-8") as f:
-        return yaml.safe_load(f)
 
 def iter_metadata_file(path, num_columns):
     with open(path, encoding="utf-8") as f:
